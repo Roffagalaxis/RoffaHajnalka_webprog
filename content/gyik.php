d4d45f273337192be8bca7790abc4e595f5905c3
Roffa Hajnalka <49552341+Roffagalaxis@users.noreply.github.com>
2019-04-14 20:32:47 +0200

    Publikálás Githubra

---
+++ content/gyik.php
@@ -0,0 +1,21 @@
+	<h1>
+		GYIK
+	</h1>
+		<section class="gyik_block">
+			<h2>
+				Miért van ez meg az?
+			</h2>
+			<p><strong>Válasz: </strong> Mert a kis nyúl szőrös és nem borotválkozik</p>
+			<h2>
+				Miért van ez meg az?
+			</h2>
+			<p><strong>Válasz: </strong> Mert a kis nyúl szőrös és nem borotválkozik</p>
+			<h2>
+				Miért van ez meg az?
+			</h2>
+			<p><strong>Válasz: </strong> Mert a kis nyúl szőrös és nem borotválkozik</p>
+			<h2>
+				Miért van ez meg az?
+			</h2>
+			<p><strong>Válasz: </strong> Mert a kis nyúl szőrös és nem borotválkozik</p>
+		</section>
